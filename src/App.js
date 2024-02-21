3f8796cda040b555c41ad5fc3dcdcab78e42a617
xliu614 <vivianne.liu@gmail.com>
2024-02-20 21:44:49 -0800

    useState to change BookCreate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 const App = () => {
-    const [books, setBooks] = useState();
-    return <div>App is here</div>
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+          console.log("Need to add book with: ", title);
+    }
+
+    return <div>
+        <BookCreate onCreate={createBook}/>
+    </div>
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,25 @@
-const BookCreate = () => {
-   return <div>BookCreate</div>
+import { useState } from "react"
+
+const BookCreate = ({onCreate}) => {
+   const [title, setTitle] = useState('');
+
+   const handleAddTitle = (event) => {
+       setTitle(event.target.value)
+   }
+
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      onCreate(title);
+      setTitle('');
+   }
+
+   return <div>
+    <form onSubmit= {handleSubmit} className="book-create">
+        <label>Title</label>
+        <input type="text" value={title} onChange={handleAddTitle}/>
+        <button>Create!</button>
+    </form>
+   </div>
 }
 
 export default BookCreate
